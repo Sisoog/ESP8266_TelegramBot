acf0c5cdf51ddbbfed6985a4a87f21cd68592d45
Zeus <Zeus@sisoog.com>
2017-07-11 15:00:31 +0430

    Add Eclipse Project File

---
+++ .ino.cpp
@@ -0,0 +1,19 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-07-10 12:56:48
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include "ZeusTgBot.h"
+void setup() ;
+void Tg_Message_Prossess(uint32_t mgs_id,uint32_t sender_id,String msg) ;
+void loop() ;
+
+#include "ESP_Telegram.ino"
+
+
+#endif
